df0804fc570c285152c8e31849685aa671f78c23
Charlieeeee <charlieworld2008@gmail.com>
2020-03-16 14:58:23 +0800

    improvement(all): adjust all layout

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,9 @@
-
-document.body.onscroll = function() {scrollEvent()};
-document.body.onresize = function() {resizeEvent()};
+document.body.onscroll = function() {
+  scrollEvent();
+};
+document.body.onresize = function() {
+  resizeEvent();
+};
 var menuCloseBtn = document.getElementById("navbarClose");
 var menuBtn = document.getElementById("navbarMobileMenu");
 var mainLogo = document.getElementById("mainLogo");
@@ -8,24 +11,30 @@ var mainLogoImg1 = document.getElementById("mainLogoImg1");
 var mainLogoImg2 = document.getElementById("mainLogoImg2");
 
 var text = document.getElementsByClassName("nav-item");
-var menuLink = ['#product', '#application', '#specification', '#certification',  '#about', 'english']
+var menuLink = [
+  "#product",
+  "#application",
+  "#specification",
+  "#certification",
+  "#about",
+  "english"
+];
 
-for (var i=0;i<text.length;i++) {
-  text[i].addEventListener("click", function(e){
+for (var i = 0; i < text.length; i++) {
+  text[i].addEventListener("click", function(e) {
     closeMenu();
   });
 }
 
-menuCloseBtn.addEventListener("click", function(e){
+menuCloseBtn.addEventListener("click", function(e) {
   closeMenu();
 });
 
-mainLogo.addEventListener("click", function(e){
+mainLogo.addEventListener("click", function(e) {
   closeMenu();
 });
 
-
-menuBtn.addEventListener("click", function(e){
+menuBtn.addEventListener("click", function(e) {
   var menu = document.getElementById("navbarSupportedContent");
   menu.classList.add("show");
   menuBtn.style.display = "none";
@@ -41,13 +50,34 @@ function closeMenu() {
 
 function scrollEvent() {
   var header = document.getElementById("nav_custom");
-  
+  var bodyScrollTop = document.body.scrollTop;
+  var elementScrollTop = document.documentElement.scrollTop;
+  var viewHeight = document.getElementById("carousel").offsetHeight;
 
+  var applicationOffsetHeight = document.getElementById("application")
+    .offsetHeight;
+  var applicationTitleMobile = document.getElementById(
+    "applicationTitleMobile"
+  );
+
+  console.log("bodyScrollTop", bodyScrollTop);
+  console.log("elementScrollTop", elementScrollTop);
+  console.log("applicationOffsetHeight", viewHeight + applicationOffsetHeight);
   if (getWindowWidth() < 768) {
     //mobile title
     displayMobileTitle();
+
+    if (
+      (bodyScrollTop > viewHeight * 2 ||
+        elementScrollTop > viewHeight * 2) &&
+      ( elementScrollTop < viewHeight + applicationOffsetHeight +220)
+    ) {
+      applicationTitleMobile.classList.add("show-opacity");
+    } else {
+      applicationTitleMobile.classList.remove("show-opacity");
+    }
   } else {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    if (bodyScrollTop > 300 || elementScrollTop > 300) {
       displayPc2Title();
       header.classList.add("nav-custom-fixed-top");
       mainLogoImg1.classList.add("hidden");
@@ -63,7 +93,7 @@ function scrollEvent() {
 
 function displayMobileTitle() {
   var text = document.getElementsByClassName("nav-item");
-  for (var i=0;i<text.length;i++) {
+  for (var i = 0; i < text.length; i++) {
     text[i].classList.remove("nav-text-pc-1");
     text[i].classList.remove("nav-text-pc-2");
     text[i].classList.add("nav-text-moble");
@@ -72,7 +102,7 @@ function displayMobileTitle() {
 
 function displayPc1Title() {
   var text = document.getElementsByClassName("nav-item");
-  for (var i=0;i<text.length;i++) {
+  for (var i = 0; i < text.length; i++) {
     text[i].classList.remove("nav-text-moble");
     text[i].classList.remove("nav-text-pc-2");
     text[i].classList.add("nav-text-pc-1");
@@ -81,7 +111,7 @@ function displayPc1Title() {
 
 function displayPc2Title() {
   var text = document.getElementsByClassName("nav-item");
-  for (var i=0;i<text.length;i++) {
+  for (var i = 0; i < text.length; i++) {
     text[i].classList.remove("nav-text-moble");
     text[i].classList.remove("nav-text-pc-1");
     text[i].classList.add("nav-text-pc-2");
@@ -89,7 +119,6 @@ function displayPc2Title() {
 }
 
 function resizeEvent() {
-
   if (getWindowWidth() >= 768) {
     menuBtn.style.display = "none";
   } else {
@@ -97,21 +126,21 @@ function resizeEvent() {
     mainLogoImg2.classList.remove("hidden");
     menuBtn.style.display = "block";
     closeMenu();
-  }  
+  }
 }
 
 function getWindowHeight() {
-  return  window.innerHeight;
+  return window.innerHeight;
 }
 
-function getWindowWidth() { 
-  return  window.innerWidth;
+function getWindowWidth() {
+  return window.innerWidth;
 }
 
-function linkNav (link) {
+function linkNav(link) {
   location.href = link;
   closeMenu();
 }
 
-resizeEvent ();
+resizeEvent();
 scrollEvent();
